0b29feb898f864cea6c4a70d05be94758fa14727
sebalr <larrieu.sebastian@gmail.com>
2019-08-13 19:09:20 -0300

    Header component

---
+++ projects/base-components/src/lib/header/header.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SidebarService } from '../sidebar/sidebar.service';
+import { BaseComponentsService } from '../base-components.service';
+import { SidebarMobileService } from '../sidebar-mobile/sidebar-mobile.service';
+
+@Component({
+  selector: 'slr-header',
+  templateUrl: './header.component.html'
+})
+export class HeaderComponent implements OnInit {
+
+  @Input() public dropdownActive = false;
+  @Input() public colapsableSidebar = false;
+
+  constructor(public coreService: BaseComponentsService, private sidebarService: SidebarService,
+    private sidebarMobileService: SidebarMobileService) { }
+
+  ngOnInit() {
+  }
+
+  public toggleSidebar() {
+    this.sidebarService.toggleSidebar.next();
+  }
+
+  public back(): void {
+    this.coreService.back();
+  }
+
+  public getName(): string {
+    return this.coreService.getName();
+  }
+
+  public toggleMobileSidebar() {
+    this.sidebarMobileService.toggleSidebar.next(true);
+  }
+}
